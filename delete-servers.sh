2c91631e3b6b32a17e23306cd8b17068ee72fe41
Matt Fine <17680862+chewrocca@users.noreply.github.com>
2019-05-20 11:37:28 -0500

    fixing gathering number of vms

--- delete-servers.sh
+++ delete-servers.sh
@@ -6,13 +6,10 @@ if [[ ! $1 =~ ^(virtualbox|do|gce)$ ]]; then
   exit 1
 fi
 
-# Get number of machines
-echo "Getting number of machines..."
-GCE_MACHINES=$(docker-machine ls | grep -c gce)
-DO_MACHINES=$(docker-machine ls | grep -c do)
-VB_MACHINES=$(docker-machine ls | grep -c vb)
-
 if [ $1 == virtualbox ]; then
+    # Get number of machines
+    echo "Getting number of machines..."
+    VB_MACHINES=$(docker-machine ls | grep -c vb)
     echo "Deleting VirtualBox machines..."
     # delete all docker machines starting with vb
     for server in $(seq 1 $VB_MACHINES); do
@@ -22,6 +19,9 @@ if [ $1 == virtualbox ]; then
 fi
 
 if [ $1 == do ]; then
+    # Get number of machines
+    echo "Getting number of machines..."
+    DO_MACHINES=$(docker-machine ls | grep -c do)
     echo "Deleting Digital Ocean machines..."
     # delete all docker machines starting with do
     for server in $(seq 1 $DO_MACHINES); do
@@ -31,6 +31,9 @@ if [ $1 == do ]; then
 fi
 
 if [ $1 == gce ]; then
+    # Get number of machines
+    echo "Getting number of machines..."
+    GCE_MACHINES=$(docker-machine ls | grep -c gce)
     echo "Deleting Google Compute Engine machines..."
     # delete all docker machines starting with gce
     for server in $(seq 1 $GCE_MACHINES); do
